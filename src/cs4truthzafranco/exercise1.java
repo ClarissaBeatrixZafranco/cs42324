c89abeb9b9e31d821a25da0071b9314f0967dbbc
ClarissaBeatrixZafranco <143586103+ClarissaBeatrixZafranco@users.noreply.github.com>
2023-09-05 14:30:18 +0800

    Add files via upload

---
+++ src/cs4truthzafranco/exercise1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4truthlecture;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class exercise1 {
+    String racer1Name = "Strip 'The King' Weathers";
+    String racer1Sponsor = "Dinoco";
+    int racer1PistonCups = 7;
+    
+    String racer2Name = "Chick Hicks";
+    String racer2Sponsor = "Hostile Takeover Bank";
+    int racer2PistonCups = 1;
+    
+    String racer3Name = "Lightning McQueen";
+    String racer3Sponsor = "Rust-eze";
+    int racer3PistonCups = 7;
+}
